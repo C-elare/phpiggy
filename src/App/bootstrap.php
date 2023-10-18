12cd63f56655cb307dcc66cb07571c7463c7a3cc
C_elare <houvillev@gmail.com>
2023-10-18 20:27:26 +0200

    Add Dispatch, REGEX, Instant class

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Controllers\HomeController;
+
+
 
 $app = new App();
 
-$app->get('/', ['App\Controllers\HomeController']);
+$app->get('/', [HomeController::class, 'home']);
 
 
-
-dd($app);
 
 return $app;
