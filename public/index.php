f022cfb623068a79688e4611bcd1f1342e84136e
C_elare <houvillev@gmail.com>
2023-10-21 12:27:56 +0200

    git add . oco

--- public/index.php
+++ public/index.php
@@ -5,5 +5,5 @@ include __DIR__ . "/../src/App/functions.php";
 // Include and get the app instance
 $app = include __DIR__ . "/../src/App/bootstrap.php";
 
-// Run the application s
+// Run the applicationsss
 $app->run();
