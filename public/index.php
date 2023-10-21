286a03190effd443cc5ef9334c3eeaf8cfe158b8
C_elare <houvillev@gmail.com>
2023-10-21 12:22:16 +0200

    @opencommit

--- public/index.php
+++ public/index.php
@@ -5,5 +5,5 @@ include __DIR__ . "/../src/App/functions.php";
 // Include and get the app instance
 $app = include __DIR__ . "/../src/App/bootstrap.php";
 
-// Run the application
+// Run the application s
 $app->run();
